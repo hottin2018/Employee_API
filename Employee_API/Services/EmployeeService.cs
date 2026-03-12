ad90cca0ee502b3aafb74a505c5302514d5c2323
Your Name <you@example.com>
2026-03-12 19:59:05 -0400

    ServiceImplemnetd

--- Employee_API/Services/EmployeeService.cs
+++ Employee_API/Services/EmployeeService.cs
@@ -1,10 +1,30 @@
-﻿namespace Employee_API.Services
+﻿using Employee_API.Models;
+using Employee_API.Repositories;
+namespace Employee_API.Services
 {
     public class EmployeeService
     {
-        public EmployeeService()
+        private readonly IEmployeeRepository _repo;
+        public EmployeeService(IEmployeeRepository repository)
         {
-            
+            _repo = repository;
+        }
+
+        public async Task<List<Employee>> GetEmployeesAsync()
+        {
+            return await _repo.GetEmployees();
+        }
+        public async Task<Employee> GetEmploeeById(int id)
+        {
+            return await _repo.GetEmplyeeById(id);
+        }
+        public async Task UpdateEmployeeAsync(Employee employee)
+        {
+            await _repo.UpdateEmployee(employee);
+        }
+        public async Task DeleteEmployee(int id)
+        {
+            await _repo.DeleteEmployee(id);
         }
     }
 }
